357383d65c103709c79d75efc6f66415235f1b1b
maxwelldvg <maxwelldvg@gmail.com>
2024-10-12 00:22:26 +0200

    Extremely crude oscilloscope

---
+++ visualizer.py
@@ -0,0 +1,60 @@
+# Copyright Max Van Gelder 2024
+# This code is garbage idgaf I just want to vibe with pretty colors and stuff
+# Maybe I'll make it actually good code later maybe not
+
+import pydub
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+MUSIC_FILE = "/home/mvg/Music/q.mp3"
+FRAME_RATE = 24 # fps for animaton
+WINDOW_LEN = 300 # window duration in MS
+NUM_SECS = 30 # how many seconds to generate animation for
+SMOOTHING_KERNAL_LEN = 20 # for finding peaks and valley in the trigger detection using uniform smoothing
+
+song = pydub.AudioSegment.from_mp3(MUSIC_FILE)
+
+# I ripped this function straight from a stackoverflow answer
+def pydub_to_np(audio: pydub.AudioSegment) -> tuple[np.ndarray, int]:
+    """
+    Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
+    where each value is in range [-1.0, 1.0]. 
+    Returns tuple (audio_np_array, sample_rate).
+    """
+    return np.array(audio.get_array_of_samples(), dtype=np.float32).reshape((-1, audio.channels)) / (
+            1 << (8 * audio.sample_width - 1)), audio.frame_rate
+
+song = pydub_to_np(song)
+#TODO: dual channels
+song_array: np.ndarray = (song[0][:, 0] + song[0][:, 1]) / 2 # IK theres a better way idgaf
+song_array = song_array[:song[1] * 5] # first five seconds
+
+smoothing_kernel = np.ones(SMOOTHING_KERNAL_LEN) / SMOOTHING_KERNAL_LEN
+smoothed_song = np.convolve(song_array, smoothing_kernel, mode='same')
+
+def update(frame: int):
+    # Find nearest trigger point (looking backwards only)
+    frame_to_time = lambda f: int(1000 * (f / FRAME_RATE))
+    frame_start_time = frame_to_time(frame)
+    prev_frame_start_time = frame_to_time(frame - 1)
+    if prev_frame_start_time < 0:
+        line.set_ydata(song_array[frame_start_time:frame_start_time + WINDOW_LEN])
+        return line,
+    for sample_time in range(prev_frame_start_time, frame_start_time):
+        if smoothed_song[sample_time] > smoothed_song[sample_time- 2] and smoothed_song[sample_time] > smoothed_song[sample_time - 1] and \
+           smoothed_song[sample_time] > smoothed_song[sample_time + 1] and smoothed_song[sample_time] > smoothed_song[sample_time + 2]:
+                line.set_ydata(song_array[frame_start_time:frame_start_time + WINDOW_LEN])
+    return line,
+
+fig, ax = plt.subplots()
+
+# Initialize the plot (empty line)
+line, = ax.plot(np.zeros(WINDOW_LEN))
+ax.set_ylim(-1, 1) # Signal is normalized -1.0 to 1.0 so this is sufficient
+
+# Create the animation
+ani = animation.FuncAnimation(fig, update, frames=24 * NUM_SECS, interval=1000 / FRAME_RATE)
+
+# Save the animation as a video
+ani.save('animation.mp4', writer='ffmpeg')
