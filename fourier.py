2c686708c0443fda013bf0c2b5dadeb9654823f7
maxwelldvg <maxwelldvg@gmail.com>
2024-10-19 16:41:40 +0200

    works with wav files now

--- fourier.py
+++ fourier.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
 if __name__ == "__main__":
-    # Two sin waves on top of each other of different frequencies
-    # We will pretend this is a musical recording that lasts 2 seconds
-    SAMPLING_RATE = 1000 # Hz, this is arbitrary make-believe
+    WAV_FNAME = "/home/mvg/Music/bon.wav"
+    samplerate, data = wavfile.read(WAV_FNAME)
     DURATION = 2 # seconds
-    x = np.linspace(0, DURATION, DURATION * SAMPLING_RATE) # make a 2 second long "recording"
-    sin = np.sin(5 * np.pi * x) # make a 2.5Hz sine wave w/ magnitude 1
-    quick_sin = .15 * np.sin(50 * np.pi * x) # make a 25Hz sine wave w/ magnitude .15
-    vec = sin + quick_sin
+    START_TIME = 14 # seconds
+    x = np.linspace(START_TIME, START_TIME + DURATION, DURATION * samplerate)
+    left_channel = data[:,1]
+    vec = left_channel[START_TIME * samplerate:samplerate * (START_TIME + DURATION)]
 
     # Configure the plot
     plt.figure(figsize=(10, 6))  # Set the figure size
@@ -30,24 +30,10 @@ if __name__ == "__main__":
     magnitudes = np.abs(fourier_transform)
     max_index = np.argmax(magnitudes)
     # get frequency bin centers
-    frequency_resolution = np.fft.rfftfreq(len(vec), 1 / SAMPLING_RATE)
+    frequency_resolution = np.fft.rfftfreq(len(vec), 1 / samplerate)
     dominant_frequency = frequency_resolution[max_index]
     second_dominant_frequency = frequency_resolution[np.argpartition(magnitudes, -2)[-2]]
     print(f"Dominant frequency: {dominant_frequency}")
     print(f"Secondary frequency: {second_dominant_frequency}")
-
-    # # Plot the Fourier transform as a histogram
-    # plt.figure(figsize=(10, 6))
-    # plt.hist(np.abs(fourier_transform), bins=1000, color='blue', alpha=0.7)
-    # plt.xlim(1, 20)  # Set the x-axis limits to 1 and 20
-    # plt.xlabel('Frequency')
-    # plt.ylabel('Magnitude')
-    # plt.title('Fourier Transform Histogram')
-    # plt.grid(True)
-    # plt.show()
-
-
-    # # Show the plot
-    # plt.show()
 
     print("hi!")
